53752f11381291f209cac69f38b510251bdb814f
edward0107 <edwarlopez010@gmail.com>
2025-11-13 20:44:59 -0600

    First commit

---
+++ tiendaweb/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+path('', views.inicio, name='inicio'),
+path('categorias/', views.categorias, name='categorias'),
+path('productos/', views.productos, name='productos'),
+path('creditos/', views.creditos, name='creditos'),
+]
